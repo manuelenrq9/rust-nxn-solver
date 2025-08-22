fb93a860f558ff355349adab92236da3d2b1d702
manuelenrq9 <manuel.enrqt@gmail.com>
2025-08-22 11:58:45 -0400

    feat: :sparkles: add user input for N
    add user input for N

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the NxN calculator for systems of linear equations");
+    println!("Please write down the value for N: ");
+    let mut n_input = String::new();
+    io::stdin().read_line(&mut n_input);
+    println!("N is {}", n_input);
 }
