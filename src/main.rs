5e27f676b609792058e8c02900a98645d4d9bc05
manuelenrq9 <manuel.enrqt@gmail.com>
2025-08-25 17:25:38 -0400

    feat: :sparkles: matrix created succesfully
    matrix created succesfully

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,23 @@ use std::io;
 fn main() {
     println!("Welcome to the NxN calculator for systems of linear equations");
     println!("Please write down the value for N: ");
-    let mut n_input = String::new();
-    io::stdin().read_line(&mut n_input);
-    println!("N is {}", n_input);
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let n: u32 = input
+        .trim()
+        .parse()
+        .expect("Failed to parse number: Not a valid integer");
+
+    let augmented_matrix: Vec<Vec<i32>> = Vec::new();
+    for i in 0..n {
+        println!("Equation number:{}", i + 1);
+        for j in 0..=n {
+            println!("Please enter the coeficient for the variable {}", j + 1);
+        }
+    }
 }
