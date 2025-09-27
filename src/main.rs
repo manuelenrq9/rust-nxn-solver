20d210648793bf0bffe4e25cfd60edba1580e477
manuelenrq9 <manuel.enrqt@gmail.com>
2025-09-27 18:22:00 -0400

    fix: :bug: solved all errors related to solving the lower part of the matrix
    solved bugs related to solving the lower part of the matrix, now it works perfectly

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,17 @@ fn main() {
     println!("Welcome to the NxN calculator for systems of linear equations");
     println!("Please write down the value for N: ");
     let n: i32 = get_i32_from_user();
-    let mut matrix: Vec<Vec<i32>> = create_augmented_matrix(&n);
+    let mut matrix: Vec<Vec<f64>> = create_augmented_matrix(&n);
     show_augmented_matrix(&matrix, &n);
     println!("===============================================================");
     println!("===============================================================");
     solve_augmented_matrix(&mut matrix, n);
-    show_augmented_matrix(&matrix, &n);
+    //show_augmented_matrix(&matrix, &n);
 }
 
-fn create_augmented_matrix(n: &i32) -> Vec<Vec<i32>> {
+fn create_augmented_matrix(n: &i32) -> Vec<Vec<f64>> {
     // Create the augmented matrix as a Vector of Vectors
-    let mut augmented_matrix: Vec<Vec<i32>> = Vec::new();
+    let mut augmented_matrix: Vec<Vec<f64>> = Vec::new();
 
     // Iterate over the matrix and ask user input
     // for coefficients and the constant of each equation
@@ -22,11 +22,11 @@ fn create_augmented_matrix(n: &i32) -> Vec<Vec<i32>> {
         println!("Equation number:{}", i + 1);
 
         //Create an inner vector
-        let mut inner_vector: Vec<i32> = Vec::new();
+        let mut inner_vector: Vec<f64> = Vec::new();
 
         for j in 0..=*n {
             println!("Please enter the coeficient for the variable {}", j + 1);
-            let coefficient: i32 = get_i32_from_user();
+            let coefficient: f64 = get_f64_from_user();
 
             //assign the coefficient value into the inner vector
             inner_vector.push(coefficient);
@@ -34,7 +34,6 @@ fn create_augmented_matrix(n: &i32) -> Vec<Vec<i32>> {
         //insert the inner vector (equation) into the matrix
         augmented_matrix.push(inner_vector);
     }
-
     return augmented_matrix;
 }
 
@@ -44,8 +43,20 @@ fn get_i32_from_user() -> i32 {
     //get user input as a String and store it in s
     io::stdin().read_line(&mut s).expect("Failed to read line");
 
-    // Convert the user input (String) into an i32 value
+    // Convert the user input (String) into an f64 value
     let n: i32 = convert_string_to_i32(s);
+
+    return n;
+}
+
+fn get_f64_from_user() -> f64 {
+    let mut s: String = String::new();
+
+    //get user input as a String and store it in s
+    io::stdin().read_line(&mut s).expect("Failed to read line");
+
+    // Convert the user input (String) into an f64 value
+    let n: f64 = convert_string_to_f64(s);
 
     return n;
 }
@@ -59,7 +70,16 @@ fn convert_string_to_i32(s: String) -> i32 {
     return n;
 }
 
-fn show_augmented_matrix(matrix: &Vec<Vec<i32>>, n: &i32) {
+fn convert_string_to_f64(s: String) -> f64 {
+    let n: f64 = s
+        .trim()
+        .parse()
+        .expect("Failed to parse number: Not a valid integer");
+
+    return n;
+}
+
+fn show_augmented_matrix(matrix: &Vec<Vec<f64>>, n: &i32) {
     for i in 0..*n {
         for j in 0..=*n {
             print!("[{}],", matrix[i as usize][j as usize]);
@@ -68,17 +88,57 @@ fn show_augmented_matrix(matrix: &Vec<Vec<i32>>, n: &i32) {
     }
 }
 
-fn solve_augmented_matrix(matrix: &mut Vec<Vec<i32>>, n: i32) {
+fn solve_augmented_matrix(matrix: &mut Vec<Vec<f64>>, n: i32) {
+    //first we solve the lower triangle by doing forward elimination
     //set the pivot column for each column on the left side of the matrix
-    for j in 0..n {
+    for k in 0..n {
         //check if the value at [j][j](main diagonal) is different from 0
         // if it its, set it as the pivot element
-        if matrix[j as usize][j as usize] != 0 {
+        if matrix[k as usize][k as usize] != 0.0 {
             // Apply a division through the whole row, to make the pivot element equal to 1
-            let pivot_value: i32 = matrix[j as usize][j as usize];
-            for j2 in 0..=n {
-                matrix[j as usize][j2 as usize] /= pivot_value;
+            let pivot_value: f64 = matrix[k as usize][k as usize];
+            for j in 0..=n {
+                matrix[k as usize][j as usize] /= pivot_value;
             }
+            show_augmented_matrix(matrix, &n);
+            println!("===============================================================");
+            println!("===============================================================");
+            //make all values below the pivot element equal to zero,
+            //by substracting the pivot element's row multiplied by
+            //the value you wanto to turn to zero
+            for i in k + 1..n {
+                let row_multiplier: f64 = matrix[i as usize][k as usize];
+                for j in 0..=n {
+                    let substracting_value: f64 = matrix[k as usize][j as usize] * row_multiplier;
+                    matrix[i as usize][j as usize] -= substracting_value;
+                    show_augmented_matrix(matrix, &n);
+                    println!("===============================================================");
+                    println!("===============================================================");
+                }
+                show_augmented_matrix(matrix, &n);
+                println!("===============================================================");
+                println!("===============================================================");
+            }
+            show_augmented_matrix(matrix, &n);
+            println!("===============================================================");
+            println!("===============================================================");
         }
     }
+    /*
+    //backward elimination for solving the upper triangle
+    for k in n - 1..0 {
+        let pivot_value: f64 = matrix[k as usize][k as usize];
+        if pivot_value != 0 {
+            //make all values below the pivot element equal to zero,
+            //by substracting the pivot element's row multiplied by
+            //the value you wanto to turn to zero
+            for i in k - 1..0 {
+                for j in 0..n {
+                    let substracting_value: f64 =
+                        matrix[k as usize][j as usize] * matrix[i as usize][k as usize];
+                    matrix[i as usize][j as usize] -= substracting_value;
+                }
+            }
+        }
+    }  */
 }
