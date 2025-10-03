babaf43edd3abd76dd4fbb9230ac050667b180d5
manuelenrq9 <manuel.enrqt@gmail.com>
2025-10-03 15:40:51 -0400

    refactor: :mute: removed some prints for debugging
    removed some prints for debugging adn seeing matrix behavior while being solved

--- src/solve_augmented_matrix.rs
+++ src/solve_augmented_matrix.rs
@@ -1,10 +1,8 @@
-use crate::show_augmented_matrix;
-
 pub fn solve_augmented_matrix(matrix: &mut Vec<Vec<f64>>, n: i32) {
     //first we solve the lower triangle by doing forward elimination
     //set the pivot column for each column on the left side of the matrix
     for k in 0..n {
-        //check if the value at [j][j](main diagonal) is different from 0
+        //check if the value at [k][k](main diagonal) is different from 0
         // if it its, set it as the pivot element
         let pivot_value: f64 = matrix[k as usize][k as usize];
         if pivot_value != 0.0 {
@@ -12,9 +10,6 @@ pub fn solve_augmented_matrix(matrix: &mut Vec<Vec<f64>>, n: i32) {
             for j in 0..=n {
                 matrix[k as usize][j as usize] /= pivot_value;
             }
-            show_augmented_matrix::show_augmented_matrix(matrix, &n);
-            println!("===============================================================");
-            println!("===============================================================");
             //make all values below the pivot element equal to zero,
             //by substracting the pivot element's row multiplied by
             //the value you wanto to turn to zero
@@ -23,21 +18,14 @@ pub fn solve_augmented_matrix(matrix: &mut Vec<Vec<f64>>, n: i32) {
                 for j in 0..=n {
                     let substracting_value: f64 = matrix[k as usize][j as usize] * row_multiplier;
                     matrix[i as usize][j as usize] -= substracting_value;
-                    show_augmented_matrix::show_augmented_matrix(matrix, &n);
-                    println!("===============================================================");
-                    println!("===============================================================");
                 }
-                show_augmented_matrix::show_augmented_matrix(matrix, &n);
-                println!("===============================================================");
-                println!("===============================================================");
             }
-            show_augmented_matrix::show_augmented_matrix(matrix, &n);
-            println!("===============================================================");
-            println!("===============================================================");
         }
     }
     //backward elimination for solving the upper triangle
     for k in (0..n).rev() {
+        //check if the value at [k][k](main diagonal) is different from 0
+        // if it its, set it as the pivot element
         let pivot_value: f64 = matrix[k as usize][k as usize];
         if pivot_value != 0.0 {
             //make all values above the pivot element equal to zero,
