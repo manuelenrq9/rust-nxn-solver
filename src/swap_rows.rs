bdb2b64a37f87efa4c64be7005a01199d3eb5352
manuelenrq9 <manuel.enrqt@gmail.com>
2025-10-09 17:08:45 -0400

    fix: :bug: Row swapping bug solved.
    row swapping was failing because the last column was being neglected on the for loop. Now its solved.

--- src/swap_rows.rs
+++ src/swap_rows.rs
@@ -2,7 +2,7 @@ pub fn swap_rows(n: i32, pivot_index: i32, matrix: &mut Vec<Vec<f64>>) {
     let mut aux: f64;
     for i in pivot_index + 1..n - 1 {
         if matrix[i as usize][pivot_index as usize] != 0.0 {
-            for j in 0..n {
+            for j in 0..=n {
                 aux = matrix[pivot_index as usize][j as usize];
                 matrix[pivot_index as usize][j as usize] = matrix[i as usize][j as usize];
                 matrix[i as usize][j as usize] = aux;
