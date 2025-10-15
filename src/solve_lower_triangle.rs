363e0798d5e072f228670a2b0a081cb0ed11a710
manuelenrq9 <manuel.enrqt@gmail.com>
2025-10-15 12:22:48 -0400

    refactor: :construction: working on solving problems with edge case of infinite solutions and normalization.
    wok in progress, not finished. Implementing changes to the algorithm so that, systems that have infinite solutions can be normalized, and thus, solved correctly.

--- src/solve_lower_triangle.rs
+++ src/solve_lower_triangle.rs
@@ -1,5 +1,4 @@
 use crate::pivot_row_division;
-use crate::show_augmented_matrix;
 use crate::solve_lower_column;
 use crate::swap_rows;
 
@@ -7,20 +6,16 @@ pub fn solve_lower_triangle(matrix: &mut Vec<Vec<f64>>, n: i32) {
     for pivot_index in 0..n {
         //check if the value at [pivot_index][pivot_index] (main diagonal) is different from 0
         // if it its, set it as the pivot element, else, swap the row with another one below it
+
         let mut pivot_value: f64 = matrix[pivot_index as usize][pivot_index as usize];
         if pivot_value == 0.0 {
             swap_rows::swap_rows(n, pivot_index, matrix);
             pivot_value = matrix[pivot_index as usize][pivot_index as usize];
+            if pivot_value == 0.0 {}
         }
         // Apply a division through the whole row, to make the pivot element equal to 1
-        pivot_row_division::pivot_row_division(n, pivot_index, pivot_value, matrix); //problem here, carefull with division by zero. 
-        show_augmented_matrix::show_augmented_matrix(matrix, &n);
-        println!("===============================================================");
-        println!("===============================================================");
+        pivot_row_division::pivot_row_division(n, pivot_index, pivot_value, matrix);
         //solve lower column values
-        solve_lower_column::solve_lower_column(n, pivot_index, matrix, pivot_value);
-        show_augmented_matrix::show_augmented_matrix(matrix, &n);
-        println!("===============================================================");
-        println!("===============================================================");
+        solve_lower_column::solve_lower_column(n, pivot_index, matrix);
     }
 }
--- src/solve_upper_triangle.rs
+++ src/solve_upper_triangle.rs
@@ -3,9 +3,8 @@ use crate::solve_upper_column;
 
 pub fn solve_upper_triangle(matrix: &mut Vec<Vec<f64>>, n: i32) {
     for pivot_index in (0..n).rev() {
-        let pivot_value: f64 = matrix[pivot_index as usize][pivot_index as usize];
         //solve upper triangle
-        solve_upper_column::solve_upper_column(n, pivot_index, matrix, pivot_value);
+        solve_upper_column::solve_upper_column(n, pivot_index, matrix);
         show_augmented_matrix::show_augmented_matrix(matrix, &n);
         println!("===============================================================");
         println!("===============================================================");
