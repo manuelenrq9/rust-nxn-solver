c5ef08802f868b09c9d1bfc28837627606139652
manuelenrq9 <manuel.enrqt@gmail.com>
2025-10-18 18:59:29 -0400

    feat: :children_crossing: now all the numbers shown on the augmented matrix have two decimal points.
    now all the numbers shown on the augmented matrix have two decimal points. This is done so that the matrix stays squared when printed and the collumns are aligned. Also, numbers now are better rounded, so the results are more clear.

--- src/show_augmented_matrix.rs
+++ src/show_augmented_matrix.rs
@@ -4,7 +4,7 @@ pub fn show_augmented_matrix(matrix: &Vec<Vec<f64>>, n: i32) {
     for i in 0..n {
         for j in 0..=n {
             print!(
-                "[{}],",
+                "[{:.2}],",
                 epsilon_check::epsilon_check(matrix[i as usize][j as usize])
             );
         }
